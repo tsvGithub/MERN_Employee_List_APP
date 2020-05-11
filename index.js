ecf3ea555d3148034fe48d952a3ca70ad33c066d
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-05-11 17:10:15 +0300

    for heroku

--- index.js
+++ index.js
@@ -12,10 +12,24 @@ app.use(bodyParser.json());
 const employee = require("./routes/employee");
 //for express app to use route
 app.use("/employee", employee);
-//----------------------------------------
-//MongoDB Atlass connection
+
+//-------------------------------
+//Deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const uri = process.env.mongodb || "mongodb://localhost:27017/mernstack";
+//-----------------------------------------
 mongoose
-  .connect("mongodb+srv://only4Me:only4MeDB@cluster0-ztzuu.mongodb.net/mernstack?retryWrites=true&w=majority", {
+  //connect to local DB with name 'mernstack'
+  // .connect("mongodb://localhost:27017/mernstack",
+  //or connect to MongoDb Atlas
+  // .connect("mongodb+srv://only4Me:only4MeDB@cluster0-ztzuu.mongodb.net/mernstack?retryWrites=true&w=majority", {
+  .connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -28,9 +42,14 @@ mongoose
   });
 
 //----------------------------------------
+// Step 3
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 //first for deploying; second for developing
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+// const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
 });
